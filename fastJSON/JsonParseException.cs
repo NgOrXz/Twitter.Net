0607c9b4b6069a59541d6b1afcf7b8632e53685b
Kevin Wang <tsubasa.xw@gmail.com>
2012-04-01 07:35:38 +0800

    commit

---
+++ fastJSON/JsonParseException.cs
@@ -0,0 +1,20 @@
+﻿namespace fastJSON
+{
+    using System;
+
+    [Serializable]
+    public sealed class JsonParseException : Exception
+    {
+        public JsonParseException(string message)
+            : this(message, null)
+        {
+            
+        }
+
+        public JsonParseException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+            
+        }
+    }
+}
